2bfbc4f77f9bae01ccd99427e420e5cb50b93422
T3130 <wadsf506470@gmail.com>
2020-04-21 12:15:49 +0800

    自訂回應，HTTP狀態碼預設200，可以設定ResponseEntity物件並設定狀態碼

--- src/main/java/com/alangroup/controller/ProductController.java
+++ src/main/java/com/alangroup/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.alangroup.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,13 @@ import com.alangroup.vo.Product;
 public class ProductController {
 
 	@GetMapping(value="/products/{id}")
-	public Product getProduct(@PathVariable("id")String id) {
+	public ResponseEntity<Product> getProduct(@PathVariable("id")String id) {
 		Product product = new Product();
 		product.setId(id);
 		product.setName("Horror movie episode" + id);
-		return product;
+//		return ResponseEntity.ok().body(product);
+// 回傳ResponseEntity時，可以選擇一些常見的HTTP狀態，例如200（OK）、201（Created）、404（Not Found）、422（Unprocessable Entity）等。
+		return new ResponseEntity<>(product,HttpStatus.NOT_FOUND);
 	}
 	
 }
