fdc4664f6c1fa061f64fbe932d6456571ca8216d
T3130 <wadsf506470@gmail.com>
2020-04-21 11:30:54 +0800

    建立springboot

---
+++ src/test/java/com/alangroup/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alangroup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
